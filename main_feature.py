43fe22d8492352185d93c8088cd8da8f83851b3f
shashanksbharadwaj161 <132832605+shashanksbharadwaj161@users.noreply.github.com>
2023-09-09 11:29:15 +0530

    Update main_feature.py

--- main_feature.py
+++ main_feature.py
@@ -9,10 +9,10 @@ class FeatureExtractor:
         self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
 
     def extract(self, img):
-        img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
-        img = img.convert('RGB')  # Making sure img is color
-        x = image.img_to_array(img)  # To np.array. Height x Width x Channel. dtype=float32
-        x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C), where the first elem is the number of img
-        x = preprocess_input(x)  # Subtracting avg values for each pixel
-        feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
-        return feature / np.linalg.norm(feature)  # Normalize
+        img = img.resize((224, 224))  
+        img = img.convert('RGB') 
+        x = image.img_to_array(img)  
+        x = np.expand_dims(x, axis=0) 
+        x = preprocess_input(x)  
+        feature = self.model.predict(x)[0]  
+        return feature / np.linalg.norm(feature)  
