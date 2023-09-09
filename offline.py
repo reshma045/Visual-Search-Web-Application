23bc54133474531860eff37f6c8b9c3f4d54a355
shashanksbharadwaj161 <132832605+shashanksbharadwaj161@users.noreply.github.com>
2023-09-09 11:27:25 +0530

    Update offline.py

--- offline.py
+++ offline.py
@@ -6,8 +6,8 @@ import numpy as np
 if __name__ == '__main__':
     fe = FeatureExtractor()
 
-    for img_path in sorted(Path("./static/img").glob("*.jpg")):
-        print(img_path)  # e.g., ./static/img/xxx.jpg
+    for img_path in sorted(Path("./static/img").glob("*.png")):
+        print(img_path)  
         feature = fe.extract(img=Image.open(img_path))
-        feature_path = Path("./static/feature") / (img_path.stem + ".npy")  # e.g., ./static/feature/xxx.npy
+        feature_path = Path("./static/feature") / (img_path.stem + ".npy")  
         np.save(feature_path, feature)
